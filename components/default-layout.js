05c2f506f028a93c34fbf7100d312ce4e2bf3e36
Contradicted <samee.aslam22@gmail.com>
2024-07-01 13:27:41 +0100

    build: initial commit, add dashboard layout

---
+++ components/default-layout.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import Sidebar from "@/components/sidebar";
+import Header from "@/components/header";
+
+export default function DefaultLayout({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex min-h-screen overflow-hidden">
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+
+      <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <Header isOpen={isOpen} setIsOpen={setIsOpen} />
+
+        <main>
+          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
